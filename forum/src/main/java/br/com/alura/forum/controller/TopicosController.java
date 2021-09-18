514c9872b8ca44f1243363590d254380f2b9d2d1
NandaMarsol <fernanda.almferreira@gmail.com>
2021-09-18 15:09:06 -0300

    Criando endpoint para remover um Tópico

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,6 +75,14 @@ public class TopicosController {
 		Topico topico = atualizacaoTopicoRequest.atualizar(id, topicoRepository);
 		
 		return ResponseEntity.ok(new TopicoDto(topico));
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+		topicoRepository.deleteById(id);
+		
+		return ResponseEntity.ok().build(); // não devolve nenhum corpo na resposta. vou deixar o ok()vazio, já que só quero devolver o "200 OK".
+		
 	}
 	
 }
