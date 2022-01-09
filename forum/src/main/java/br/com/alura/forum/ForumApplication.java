8accea2b070aa67d433e1d5e8053d4c0a0b98df4
NandaMarsol <fernanda.almferreira@gmail.com>
2022-01-09 16:31:23 -0300

    simplificando a paginacao e ordenacao

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport // habilita suporte para o Spring pegar da requisição, os parâmetros da url / campos, as informações de paginação e ordenação, e repassar isso para o Spring data
 public class ForumApplication {
 
 	public static void main(String[] args) {
